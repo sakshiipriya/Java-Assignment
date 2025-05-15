455e0c2a4402818014006287aaace5f4c224c3c0
sakshiipriya <priyasakshi240@gmail.com>
2025-05-15 20:38:37 +0530

    Update third.java

--- Assignment-1/third.java
+++ Assignment-1/third.java
@@ -1,8 +1,8 @@
 public class third {
     public static void main(String args[]){
    student s1=new student();
-   s1.name="Vidhi";
-   s1.rollno=49;
+   s1.name="Ram";
+   s1.rollno=101;
    s1.display();
 
     }
